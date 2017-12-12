78d9f16fcab696adcf29d2b986ed51fee4fc8b30
Pablo Paletto <pablo.paletto@gmail.com>
2017-12-12 12:09:53 -0300

    New Branch

--- TP4.py
+++ TP4.py
@@ -6,6 +6,7 @@ Created on Fri Dec  1 08:51:47 2017
 """
 import numpy as np
 import matplotlib.pylab as plt
+import time as tiempo
 
 # Caracteristicas del material
 cp=0.11 # Calor especifico
@@ -19,7 +20,7 @@ L=5 # En cmts. dimension del lado de la placa
 N=20 # Cantidad de puntos (discretizacion)
 dx=dy=L/N
 time=20
-dt=0.5
+dt=0.2
 E=alpha*dt/dx**2 # Factor de Estabilidad
 
 # Formo la Matriz inicial con las condiciones iniciales
@@ -28,24 +29,34 @@ P=np.pad(P, pad_width=1, mode='constant', constant_values=0)
 P[1,N] = 200
 P[N,1] = 20
 Temp=np.zeros((N+2,N+2))
- 
+
 # Matriz de resultados
 sol=[]
 sol.append(P) #guardo matriz inicial
 
+fig = plt.gcf()
+fig.show()
+fig.canvas.draw()
+
+plt.imshow(P,'hot')
+fig.canvas.draw()
+
 # Nucleo
 while t<time:
-    plt.imshow(P,'hot')
-    plt.show()
+
     for i in range(1,N+1):
         for j in range(1,N+1):
-            Temp[i,j]=E*(P[i-1,j]+P[i+1,j]-2*P[i,j]+P[i,j-1]+P[i,j+1]) 
+            #Temp[i,j] = P[i, j] - (E*dt/dx*(P[i,j] - P[i-1,j]))-(E*dt/dy*(P[i,j]-P[i,j-1]))
+            Temp[i,j]=E*(P[i-1,j]+P[i+1,j]-4*P[i,j]+P[i,j-1]+P[i,j+1])
             Temp[1,N] = 200
             Temp[N,1] = 20
     P=Temp
-    sol.append(P) 
-    Temp=np.zeros((N+2,N+2))       
+    sol.append(P)
+    Temp=np.zeros((N+2,N+2))
     t+=dt
+    #tiempo.sleep(0.5)
 
-plt.imshow(P,'hot')
-plt.show()
+    plt.imshow(P,'hot')
+    fig.canvas.draw()
+
+tiempo.sleep(2)
